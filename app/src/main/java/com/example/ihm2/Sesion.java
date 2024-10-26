3807ea3ee8bc5d89959dcefbfaa58fd5a7d60011
Tony1234765 <perezflorescarlosantonio@gmail.com>
2024-10-26 16:49:21 -0600

    Inicio de sesión con credenciales predefinidas

--- app/src/main/java/com/example/ihm2/Sesion.java
+++ app/src/main/java/com/example/ihm2/Sesion.java
@@ -4,36 +4,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Sesion extends AppCompatActivity implements View.OnClickListener {
-    private Button b;
-    private Button c;
+    private Button registrar;
+    private Button entrar;
+
+    private EditText usuario, contraseña;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sesion);
-        b = (Button) findViewById(R.id.button_registrarse);
-        b.setOnClickListener(this);
+        registrar = (Button) findViewById(R.id.button_registrarse);
+        registrar.setOnClickListener(this);
 
-        c = (Button) findViewById(R.id.button_entrar);
-        c.setOnClickListener(this);
+        entrar = (Button) findViewById(R.id.button_entrar);
+        entrar.setOnClickListener(this);
+
+        usuario = (EditText) findViewById(R.id.usuario);
+        contraseña = (EditText) findViewById(R.id.password);
+
 
     }
     @Override
     public void onClick(View view) {
+        String user = usuario.getText().toString();
+        String pass = contraseña.getText().toString();
+
         // Verificamos cuál botón fue presionado
         if (view.getId() == R.id.button_registrarse) {
             // Si el botón de "Registrarse" fue presionado
             Intent intentRegistro = new Intent(Sesion.this, Registro.class);
             startActivity(intentRegistro);
-        } else if (view.getId() == R.id.button_entrar) {
+        }
+
+        else if (view.getId() == R.id.button_entrar && user.equals("Usuario")  && pass.equals("Contra")) {
+            Toast.makeText(this, "Credenciales correctas", Toast.LENGTH_SHORT).show();
             // Si el botón de "Entrar" fue presionado
             Intent intentMenu = new Intent(Sesion.this, Menu.class);
             startActivity(intentMenu);
+        }
+        else {
+            Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
         }
     }
 }
